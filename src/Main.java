9b88b21f584ca98cc28890de8b4e7186dbff1509
Andre Ferreira <andrevcandido61@gmail.com>
2023-02-18 21:32:14 -0300

    classes mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descricao do curso de Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Curso de JavaScript");
+        curso1.setDescricao("Descricao do curso de JavaScript");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descricao Mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
